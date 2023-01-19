4867999c48491e1c0a5bc06f6a70defc0c3503bb
SemirMo <Semir.Muhammad@gokstadakademiet.no>
2023-01-19 13:34:46 +0100

    jeg har bytta verdien 50 til 51

---
+++ oppgave1.py
@@ -0,0 +1,14 @@
+#1.1 - 1.3
+for x in range(51):
+  print(x)
+  #
+# 1.4
+import random
+  
+Numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,40,41,42,43,44,45,46,47,48,49]
+
+random.shuffle(Numbers)
+
+print(Numbers)
+
+# Den siste oopgaven skjønte jeg ikke helt
